b90d38df03e0aed0ab3042532f879ef1b8cbae5a
Uttam Asodariya <93051588+Uttam-Asodariya@users.noreply.github.com>
2024-04-26 20:16:26 +0200

    latest

--- .history/app_20240426162733.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__) # define application name
-
-@app.route("/", method=["GET"])
-def hello_world():
-    return "hello_world"
-
-if __name__ == "__main__"
