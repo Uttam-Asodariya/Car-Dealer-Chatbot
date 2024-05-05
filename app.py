2c0f8aeb7e2ce005faa510a645862e3c0e62edd8
Uttam Asodariya <93051588+Uttam-Asodariya@users.noreply.github.com>
2024-05-05 18:53:55 +0200

    upd

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-
+import requests
 from flask import Flask, render_template, request
 from car_dealer_chatboat import *
+from flask_apscheduler import APScheduler
 
 #waitress-serve --host 127.0.0.1 app:app for hosting app
 
 app = Flask(__name__) # define application name
+scheduler = APScheduler()
+
+def trigger_url():
+    url = "https://car-dealer-chatbot-1.onrender.com"
+    response = requests.get(url)
+    print("URL Triggered Successfully")
 
 @app.route("/", methods=["GET"])  # getting started
 def hello_world():
@@ -15,10 +22,14 @@ def communicate():
     input= request.form.get("You")
     # user_experience = "C:\Users\uttam\OneDrive\Desktop\chat_bot\stored_experience" + input
     # input.save(user_experience)
-    response = chatboat(input)
-    print(f"Chatbot Response: {response}")
-    return render_template("index.html", prediction = str(response))
+    Response = chatboat(input)
+    print(f"Chatbot Response: {Response}")
+    return render_template("index.html", prediction = str(Response))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    scheduler.init_app(app)
+    scheduler.start()
+    scheduler.add_job(id='trigger_url', func=trigger_url, trigger='interval', minutes=14)
+
+    app.run(host='0.0.0.0', debug = "True")
---
+++ read_me.md
@@ -0,0 +1,20 @@
+## Introduction
+
+- Clone this repository, install dependencies, and run the below command in the terminal.
+
+ ```
+    flask run
+ ```
+ - OR, copy below hosted link.
+
+ ```
+    https://car-dealer-chatbot-1.onrender.com
+ ```
+
+## Start with below mentioned prompt for better experience 
+-   1) Hi
+-   2) I want to buy car
+-   3) new
+
+## Disclaimer
+- This version of the chatbot can produce a false answer, as it is trained on pre-fixed intents and only gives answers to those prompts. Additionally, the boat is not for commercial use.
