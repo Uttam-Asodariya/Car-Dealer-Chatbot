5683f1d853ea8c418916e8f6e4ed5e20528ad42f
Uttam Asodariya <93051588+Uttam-Asodariya@users.noreply.github.com>
2024-05-06 20:36:56 +0200

    upd

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-import requests
+
 from flask import Flask, render_template, request
 from car_dealer_chatboat import *
-from flask_apscheduler import APScheduler
 
-#waitress-serve --host 127.0.0.1 app:app for hosting app
+#waitress-serve --host 127.0.0.1 app:app for hosting app # exclusively for windows
 
 app = Flask(__name__) # define application name
-scheduler = APScheduler()
-
-def trigger_url():
-    url = "https://car-dealer-chatbot-1.onrender.com"
-    response = requests.get(url)
-    print("URL Triggered Successfully")
 
 @app.route("/", methods=["GET"])  # getting started
 def hello_world():
@@ -22,14 +15,9 @@ def communicate():
     input= request.form.get("You")
     # user_experience = "C:\Users\uttam\OneDrive\Desktop\chat_bot\stored_experience" + input
     # input.save(user_experience)
-    Response = chatboat(input)
-    print(f"Chatbot Response: {Response}")
-    return render_template("index.html", prediction = str(Response))
-
+    response = chatboat(input)
+    print(f"Chatbot Response: {response}")
+    return render_template("index.html", prediction = str(response))
 
 if __name__ == "__main__":
-    scheduler.init_app(app)
-    scheduler.start()
-    scheduler.add_job(id='trigger_url', func=trigger_url, trigger='interval', minutes=14)
-
-    app.run(host='0.0.0.0', debug = "True")
+    app.run(debug = True)
